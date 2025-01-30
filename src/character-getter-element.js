d28de8c5d72ffa2787f2f2275b228856ae1c1b20
Rossy Lopez <rossy181071@hotmail.com>
2025-01-30 03:49:55 -0600

    Created new file

--- src/character-getter-element.js
+++ src/character-getter-element.js
@@ -1,7 +1,4 @@
 import { LitElement, css, html } from 'lit'
-import { Child1Element } from './child-1-element'
-import { CharacterGetterElement } from './character-getter-element'
-import { CharacterCardElement } from './character-card-element'
 
 /**
  * An example element.
@@ -9,62 +6,24 @@ import { CharacterCardElement } from './character-card-element'
  * @slot - This element has a slot
  * @csspart button - The button
  */
-export class MyElement extends LitElement {
-  static get properties() {
-    return {
-/**
- * The number of time the button has been clicked.
- */
-count: { type: Number },
-    }
-  }
-
-
+export class CharacterGetterElement extends LitElement {
+ 
   constructor() {
     super()
-    this.count = 0
-  }
-  myEvent1Handler() {
-    this.count++
-  }  
+   }
 
-  
-  firstUpdated() {
-   let that = this
-  
- setInterval(() =>{
-  that.shadowRoot.querySelector("#getter").getNewCharacter()
-  }, 3000)
-}
+   newRamdomInt() {
+    return Math.ceil(Math.random() * 100)
+   }
 
-  newCharacterEventHandler (e) {
-    const character = e.detail
-        
-    this.shadowRoot.querySelector("#card-1").name = character.Name
-    this.shadowRoot.querySelector("#card-1").image= character.image
-    this.shadowRoot.querySelector("#card-1").species = character.species
-    this.shadowRoot.querySelector("#card-1").status = character.status
-  }
+   getNewCharacter () {
+       fetch(`https://rickandmortyapi.com/api/character/${this.newRamdomInt()}`)
+      .then(response => response.json())
+      .then(characterData => this.dispatchEvent(new CustomEvent('new-character-event', { detail: characterData})))
+    
+   }   
+      
 
-  render() {
-    return html`
-    <div class="card">
-      <h1>
-        count is ${this.count}
-      </h1>
-      <child-1-element @my-event-1="${this.myEvent1Handler}"></child-1-element>
-      <character-getter-element id="getter" @new-character-event="${this.newCharacterEventHandler}"></character-getter-element>
-      <character-card-element id="card-1"></character-card-element>
-      <h1 id="character-name"></h1>
-      <img id="character-img">
-      <h3 id="character-species"></h3>
-      <h3 id="character-status"></h3>
-
-    </div>  
-    `
-  }
-
- 
   static get styles() {
     return css`
       :host {
@@ -89,7 +48,6 @@ count: { type: Number },
 
       .card {
         padding: 2em;
-        
       }
 
       .read-the-docs {
@@ -137,12 +95,8 @@ count: { type: Number },
           background-color: #f9f9f9;
         }
       }
-
-      img {
-        width: 50vw;
-      }
     `
   }
 }
 
-window.customElements.define('my-element', MyElement)
+window.customElements.define('character-getter-element', CharacterGetterElement)
--- src/child-1-element.js
+++ src/child-1-element.js
@@ -1,7 +1,4 @@
 import { LitElement, css, html } from 'lit'
-import { Child1Element } from './child-1-element'
-import { CharacterGetterElement } from './character-getter-element'
-import { CharacterCardElement } from './character-card-element'
 
 /**
  * An example element.
@@ -9,62 +6,17 @@ import { CharacterCardElement } from './character-card-element'
  * @slot - This element has a slot
  * @csspart button - The button
  */
-export class MyElement extends LitElement {
-  static get properties() {
-    return {
-/**
- * The number of time the button has been clicked.
- */
-count: { type: Number },
-    }
-  }
-
-
+export class Child1Element extends LitElement {
+ 
   constructor() {
     super()
-    this.count = 0
-  }
-  myEvent1Handler() {
-    this.count++
-  }  
+   }
 
-  
-  firstUpdated() {
-   let that = this
-  
- setInterval(() =>{
-  that.shadowRoot.querySelector("#getter").getNewCharacter()
-  }, 3000)
-}
-
-  newCharacterEventHandler (e) {
-    const character = e.detail
-        
-    this.shadowRoot.querySelector("#card-1").name = character.Name
-    this.shadowRoot.querySelector("#card-1").image= character.image
-    this.shadowRoot.querySelector("#card-1").species = character.species
-    this.shadowRoot.querySelector("#card-1").status = character.status
-  }
-
-  render() {
-    return html`
-    <div class="card">
-      <h1>
-        count is ${this.count}
-      </h1>
-      <child-1-element @my-event-1="${this.myEvent1Handler}"></child-1-element>
-      <character-getter-element id="getter" @new-character-event="${this.newCharacterEventHandler}"></character-getter-element>
-      <character-card-element id="card-1"></character-card-element>
-      <h1 id="character-name"></h1>
-      <img id="character-img">
-      <h3 id="character-species"></h3>
-      <h3 id="character-status"></h3>
-
-    </div>  
-    `
-  }
-
- 
+   connectedCallback() {
+    setInterval(() => {
+      this.dispatchEvent(new CustomEvent('my-event-1'))
+    }, 3000)
+   }
   static get styles() {
     return css`
       :host {
@@ -89,7 +41,6 @@ count: { type: Number },
 
       .card {
         padding: 2em;
-        
       }
 
       .read-the-docs {
@@ -137,12 +88,8 @@ count: { type: Number },
           background-color: #f9f9f9;
         }
       }
-
-      img {
-        width: 50vw;
-      }
     `
   }
 }
 
-window.customElements.define('my-element', MyElement)
+window.customElements.define('child-1-element', Child1Element)
